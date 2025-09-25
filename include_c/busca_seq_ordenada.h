e26edb4f8c3fe945e543a61c47233c507b795d7b
Talisson Pereira de Oliveira <89871517+TalissonOliveira14@users.noreply.github.com>
2025-09-25 20:30:40 -0300

    Update busca_seq_ordenada.h

--- include_c/busca_seq_ordenada.h
+++ include_c/busca_seq_ordenada.h
@@ -2,6 +2,6 @@
 #define BUSCA_SEQ_ORDENADA_H
 
 int busca_seq_ordenada(int arr[], int n, int alvo);
-int conta_especialidades_distintas(int arr[], int n);  // FUNÇÃO QUE FALTAVA!
-
+int conta_especialidades_distintas(int arr[], int n); 
 #endif
+
